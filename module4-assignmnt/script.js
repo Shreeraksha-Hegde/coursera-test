284a2a9ae740d7313803e7348bcdaa5e1e842d39
Shreeraksha-Hegde <54854999+Shreeraksha-Hegde@users.noreply.github.com>
2020-06-18 21:49:01 +0530

    Add files via upload

---
+++ module4-assignmnt/script.js
@@ -0,0 +1,17 @@
+
+
+(function(){
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+
+for (var i=0; i<names.length; i++) {
+
+
+
+  if (firstletter==="Jhon|| Jen || Jim") {
+    // byeSpeaker.speak(names)
+  } else {
+    // helloSpeaker.speak(names)
+  }
+})();
